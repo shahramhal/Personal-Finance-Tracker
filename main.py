ba1f2cf88adc797529ba9af1112c5f406cfb18c7
shahramhal <156790792+shahramhal@users.noreply.github.com>
2024-04-02 11:45:33 +0100

    Adding load and save

--- main.py
+++ main.py
@@ -7,7 +7,22 @@ import os
 financial_data_file = "financial_data.json"
 
 def initialize():
-    """Initialize the program."""
+
     if not os.path.exists(financial_data_file):
         with open(financial_data_file, 'w') as f:
             json.dump({"income": [], "expenses": [], "savings_goals": {}}, f)
+
+def load_data():
+
+    if os.path.exists(financial_data_file):
+        with open(financial_data_file, 'r') as f:
+            return json.load(f)
+    else:
+        initialize()
+        return load_data()
+
+
+def save_data(data):
+
+    with open(financial_data_file, 'w') as f:
+        json.dump(data, f)
