33c2fc72e0a6253656b6f3f845a77d2bd2cb25b1
shahramhal <156790792+shahramhal@users.noreply.github.com>
2024-04-02 11:36:17 +0100

    Creating json file

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import json
+import os 
+
+
+
+financial_data_file = "financial_data.json"
+
+def initialize():
+    """Initialize the program."""
+    if not os.path.exists(financial_data_file):
+        with open(financial_data_file, 'w') as f:
+            json.dump({"income": [], "expenses": [], "savings_goals": {}}, f)
