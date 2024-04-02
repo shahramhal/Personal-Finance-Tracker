877a2711ad558ae370f09d36c6272f94c690bf97
shahramhal <156790792+shahramhal@users.noreply.github.com>
2024-04-02 12:54:10 +0100

    Adding income button and frames for 'expenses' and 'saving'

--- main.py
+++ main.py
@@ -56,4 +56,21 @@ def visualize_data():
 #Create Tkinter 
 root = tk.Tk()
 root.title("Personal Finance Tracker")
+
+# Creating frames 
+income_frame = tk.Frame(root)
+income_frame.pack(pady=10)
+
+expense_frame = tk.Frame(root)
+expense_frame.pack(pady=10)
+
+savings_frame = tk.Frame(root)
+savings_frame.pack(pady=10)
+
+#Creating  income widgets 
+income_label = tk.Label(income_frame, text="Enter Income Amount:")
+income_label.grid(row=0, column=0)
+
+income_entry = tk.Entry(income_frame)
+income_entry.grid(row=0, column=1)
 root.mainloop()
