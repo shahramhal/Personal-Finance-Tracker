83abf01d777149772a4a67038903b0d03690ab24
shahramhal <156790792+shahramhal@users.noreply.github.com>
2024-04-02 11:51:30 +0100

    Creating Tkinter window

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import json
 import os 
 
 
-
+#Json file 
 financial_data_file = "financial_data.json"
-
+#initializing the programm 
 def initialize():
 
     if not os.path.exists(financial_data_file):
         with open(financial_data_file, 'w') as f:
             json.dump({"income": [], "expenses": [], "savings_goals": {}}, f)
-
+#Load data 
 def load_data():
 
     if os.path.exists(financial_data_file):
@@ -21,8 +21,12 @@ def load_data():
         initialize()
         return load_data()
 
-
+#Save data 
 def save_data(data):
 
     with open(financial_data_file, 'w') as f:
         json.dump(data, f)
+
+#Create Tkinter 
+root = tk.Tk()
+root.title("Personal Finance Tracker")
