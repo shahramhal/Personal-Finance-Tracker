a7151359bddb9b80f939740e7e898b33f73a5280
shahramhal <156790792+shahramhal@users.noreply.github.com>
2024-04-03 12:40:47 +0100

    Adding Error Handling

--- gui.py
+++ gui.py
@@ -3,7 +3,6 @@ from tkinter import messagebox
 from visualisation import add_income, add_expense
 
 
-
 def setup_gui(root):
     def handle_add_income():
         try:
@@ -21,27 +20,27 @@ def setup_gui(root):
         except ValueError:
             messagebox.showerror("Error", "Invalid input for expense amount.")
 
-# Creating frames 
-income_frame = tk.Frame(root)
-income_frame.pack(pady=10)
+    # Creating frames 
+    income_frame = tk.Frame(root)
+    income_frame.pack(pady=10)
 
-expense_frame = tk.Frame(root)
-expense_frame.pack(pady=10)
+    expense_frame = tk.Frame(root)
+    expense_frame.pack(pady=10)
 
-savings_frame = tk.Frame(root)
-savings_frame.pack(pady=10)
+    savings_frame = tk.Frame(root)
+    savings_frame.pack(pady=10)
 
-#Creating  income widgets 
-income_label = tk.Label(income_frame, text="Enter Income Amount:")
-income_label.grid(row=0, column=0)
+    #Creating  income widgets 
+    income_label = tk.Label(income_frame, text="Enter Income Amount:")
+    income_label.grid(row=0, column=0)
 
-income_entry = tk.Entry(income_frame)
-income_entry.grid(row=0, column=1)
+    income_entry = tk.Entry(income_frame)
+    income_entry.grid(row=0, column=1)
 
 
-#creating expense widgets 
-expense_label = tk.Label(expense_frame, text="Enter Expense Amount:")
-expense_label.grid(row=0, column=0)
+    #creating expense widgets 
+    expense_label = tk.Label(expense_frame, text="Enter Expense Amount:")
+    expense_label.grid(row=0, column=0)
 
-expense_entry = tk.Entry(expense_frame)
-expense_entry.grid(row=0, column=1)
+    expense_entry = tk.Entry(expense_frame)
+    expense_entry.grid(row=0, column=1)
